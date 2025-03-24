92424191168c42a68a33bf8976b9ba6641a9284c
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2025-03-24 10:31:02 +0900

    클럽리스트 수집

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from sqlalchemy import text
 from itsdangerous import URLSafeSerializer
 import dotenv
@@ -46,22 +47,86 @@ async def login(
     password: str = Form(...),
     db: AsyncSession = Depends(get_db)
 ):
-    query = text("SELECT userNo FROM lionsUser WHERE userName = :username AND userPassword = password(:password)")
+    query = text("SELECT userNo, userName,userRole FROM lionsUser WHERE userId = :username AND userPassword = password(:password)")
     result = await db.execute(query, {"username": username, "password": password})
     user = result.fetchone()
     if user is None:
         return templates.TemplateResponse("login/login.html", {"request": request, "error": "Invalid credentials"})
     # 서버 세션에 사용자 ID 저장
     request.session["user_No"] = user[0]
+    request.session["user_Name"] = user[1]
+    request.session["user_Role"] = user[2]
     return RedirectResponse(url="/success", status_code=303)
 
 # 로그인 성공 페이지
 @app.get("/success", response_class=HTMLResponse)
 async def success_page(request: Request):
     user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    user_Role = request.session.get("user_Role")
     if not user_No:
         return RedirectResponse(url="/")
-    return templates.TemplateResponse("member/basic.html", {"request": request, "user_id": user_No})
+    return templates.TemplateResponse("member/mainClub.html", {"request": request, "user_No": user_No, "user_Name": user_Name, "user_Role": user_Role})
+
+@app.get("/userEdit", response_class=HTMLResponse)
+async def user_edit(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("login/userEdit.html", {"request": request, "user_No": user_No, "user_Name": user_Name})
+
+@app.get("/userHome", response_class=HTMLResponse)
+async def user_home(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("member/mainClub.html", {"request": request, "user_No": user_No, "user_Name": user_Name})
+
+@app.get("/memberList", response_class=HTMLResponse)
+async def memberList(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("member/memberList.html", {"request": request, "user_No": user_No, "user_Name": user_Name})
+
+@app.get("/clubList", response_class=HTMLResponse)
+async def clubList(request: Request, db: AsyncSession = Depends(get_db)):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    query = text("SELECT * FROM lionsClub where attrib not like '%XXXUP%'")
+    result = await db.execute(query)
+    club_list = result.fetchall()  # 클럽 데이터를 모두 가져오기
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("admin/clubList.html", {"request": request, "user_No": user_No, "user_Name": user_Name, "club_list":club_list })
+
+@app.get("/rankList", response_class=HTMLResponse)
+async def rankList(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("admin/rankList.html", {"request": request, "user_No": user_No, "user_Name": user_Name})
+
+@app.get("/dictList", response_class=HTMLResponse)
+async def dictList(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    club_list = await get_clublist(async_session)
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("admin/dictList.html", {"request": request, "user_No": user_No, "user_Name": user_Name, "club_list":club_list })
+
+@app.get("/boardManager", response_class=HTMLResponse)
+async def boardManager(request: Request):
+    user_No = request.session.get("user_No")
+    user_Name = request.session.get("user_Name")
+    if not user_No:
+        return RedirectResponse(url="/")
+    return templates.TemplateResponse("board/boardMain.html", {"request": request, "user_No": user_No, "user_Name": user_Name})
 
 # 로그아웃 처리
 @app.get("/logout")
@@ -86,3 +151,21 @@ async def mlogin(
     request.session["user_No"] = user[0]
     return RedirectResponse(url="/success", status_code=303)
 
+@app.exception_handler(StarletteHTTPException)
+async def custom_404_handler(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse(
+            "/error/errorInfo.html",  # 템플릿 파일
+            {"request": request, "error_message": "페이지를 찾을 수 없습니다."},  # 템플릿에 전달할 데이터
+            status_code=404,
+        )
+    if exc.status_code == 500:
+        return templates.TemplateResponse(
+            "/error/errorInfo.html",  # 템플릿 파일
+            {"request": request, "error_message": "시스템 내부 에러 발생."},  # 템플릿에 전달할 데이터
+            status_code=500,
+        )
+    return HTMLResponse(
+        content=f"<h1>{exc.status_code} - {exc.detail}</h1>",
+        status_code=exc.status_code,
+    )
